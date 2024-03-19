4e1518b1b26f3d9ac2ee7bf1f93b1182bf75481f
vdesyatke <nikita.andronov@gmail.com>
2024-03-19 19:56:30 +0400

    Switch to rendering index.html from wine3.xlsx

---
+++ auxiliary_functions.py
@@ -0,0 +1,12 @@
+def years_declined(years):
+    """Функция принимает число лет и возвращает строку вида
+     'число лет' с правильным склонением"""
+
+    if years > 10 and str(years)[-2:] in ('11', '12', '13', '14'):
+        return f'{years} лет'
+    elif str(years)[-1:] == '1':
+        return f'{years} год'
+    elif str(years)[-1:] in ('2', '3', '4'):
+        return f'{years} года'
+    else:
+        return f'{years} лет'
--- main.py
+++ main.py
@@ -1,4 +1,41 @@
 from http.server import HTTPServer, SimpleHTTPRequestHandler
+from jinja2 import Environment, FileSystemLoader, select_autoescape
+from datetime import date
+from auxiliary_functions import years_declined
+import pandas as pd
+
+
+env = Environment(
+    loader=FileSystemLoader('.'),
+    autoescape=select_autoescape(['html', 'xml'])
+)
+
+template = env.get_template('template.html')
+
+FOUNDATION_YEAR = 1920
+
+age_of_winery = date.today().year - FOUNDATION_YEAR
+age_of_winery = years_declined(age_of_winery)
+
+wines_df = pd.read_excel(
+    'wine3.xlsx',
+    na_values='None',
+    keep_default_na=False,
+)
+
+wines = dict()
+for category in wines_df['Категория'].unique():
+    df = wines_df[wines_df['Категория'] == category]\
+        .drop(columns=['Категория'])
+    wines[category] = df.to_dict(orient='records')
+
+rendered_page = template.render(
+    age_of_winery=age_of_winery,
+    wines=wines,
+)
+
+with open('index.html', 'w', encoding="utf8") as file:
+    file.write(rendered_page)
 
 
 server = HTTPServer(('0.0.0.0', 8000), SimpleHTTPRequestHandler)
