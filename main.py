e4d3b4de53d58ac6ddf6a9f7acd3dc0616e7ac54
vdesyatke <nikita.andronov@gmail.com>
2024-03-23 11:30:59 +0400

    Add argparse path to products database xls-file

--- main.py
+++ main.py
@@ -3,9 +3,18 @@ from jinja2 import Environment, FileSystemLoader, select_autoescape
 from datetime import date
 from auxiliary_functions import decline_years
 import pandas as pd
+import argparse
 
 
 FOUNDATION_YEAR = 1920
+
+
+def create_parser():
+    description = '''Сайт магазина авторского вина "Новое русское вино".'''
+    parser = argparse.ArgumentParser(description=description)
+    h = 'Путь к xlsx-файлу с базой данных продукции'
+    parser.add_argument('-f', '--xls_file', help=h, default='wine3.xlsx')
+    return parser
 
 
 def main():
@@ -14,13 +23,16 @@ def main():
         autoescape=select_autoescape(['html', 'xml'])
     )
 
+    parser = create_parser()
+    xls_file = parser.parse_args().xls_file
+
     template = env.get_template('template.html')
 
     age_of_winery = date.today().year - FOUNDATION_YEAR
     age_of_winery = decline_years(age_of_winery)
 
     wines_df = pd.read_excel(
-        'wine3.xlsx',
+        xls_file,
         na_values='None',
         keep_default_na=False,
     )
